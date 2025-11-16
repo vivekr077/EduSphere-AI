45f067e755dee0c653162f1f0ca972cf0936f931
vivek ranjan <vivekranjan8789@gmail.com>
2025-11-16 08:45:09 +0530

    changed gemini version

--- configs/AiModel.js
+++ configs/AiModel.js
@@ -11,7 +11,7 @@ const {
   const genAI = new GoogleGenerativeAI(apiKey);
   
   const model = genAI.getGenerativeModel({
-    model: "gemini-2.0-flash-exp",
+    model: "gemini-2.0-flash",
   });
   
   const generationConfig = {
